068ff801d6cd40b228310723d9f969c35656480d
Akshay Margale <akshayprofacc@gmail.com>
2021-12-02 17:29:04 +0530

    Add add button on appbar and at end

--- lib/main.dart
+++ lib/main.dart
@@ -16,27 +16,32 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                elevation: 5,
-                color: Colors.grey,
-                child: Text(
-                  "Chart",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+        appBar: AppBar(
+          title: Text('Flutter App'),
+          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.add))],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                child: Card(
+                  elevation: 5,
+                  color: Colors.grey,
+                  child: Text(
+                    "Chart",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  ),
                 ),
               ),
-            ),
-            UserTransactions(),
-          ],
+              UserTransactions(),
+            ],
+          ),
         ),
-      ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ));
   }
 }
